212d1fe95064882f4f886d6b5fc4ae631337e169
Bluenatic21 <b.goglidze.geologist@gmail.com>
2025-06-03 21:04:42 +0400

    test

--- quick_commit.bat
+++ quick_commit.bat
@@ -1,6 +1,12 @@
 @echo off
-set /p MSG="Введите короткое описание коммита: "
-git add -A
-git commit -m "%MSG%"
+REM Добавляем все изменения и backend_debug.log явно (если вдруг был проигнорирован)
+git add .
+git add backend_debug.log
+
+REM Просим ввести сообщение коммита
+set /p msg="Введите сообщение коммита: "
+
+git commit -m "%msg%"
 git push
+
 pause
